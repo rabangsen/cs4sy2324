d228b0b2320161c7fab49f1b83188f5604b02e0b
rabangsen <112672867+rabangsen@users.noreply.github.com>
2023-09-07 16:11:37 +0800

    mkns

--- src/cs4_ch_ang/sen/CS4_Ch_AngSen.java
+++ src/cs4_ch_ang/sen/CS4_Ch_AngSen.java
@@ -16,6 +16,45 @@ public class CS4_Ch_AngSen {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String Name1 = "Batman";
+        String StarRating1 = "5";
+        int startAttack1 = 621;
+        
+        String Name2 = "Superman";
+        String StarRating2 = "5";
+        int startAttack2 = 676;        
+        
+        String Name3 = "Green Lantern";
+        String StarRating3 = "4";
+        int startAttack3 = 645;
+        
+        System.out.println("Hero 1:");
+        
+        System.out.println("Name: " + Name1);
+        System.out.println("Star Rating: " + StarRating1);
+        System.out.println("Attack Damage: " + startAttack1);
+        
+        System.out.println("Hero 2:");
+        
+        System.out.println("Name: " + Name2);
+        System.out.println("Star Rating: " + StarRating2);
+        System.out.println("Attack Damage: " + startAttack2);
+        
+        System.out.println("Hero 3:");
+        
+        System.out.println("Name: " + Name3);
+        System.out.println("Star Rating: " + StarRating3);
+        System.out.println("Attack Damage: " + startAttack3);
+        
+        boolean HigherDamage = startAttack2 > startAttack1;
+        boolean HigherStars = StarRating1 > StarRating3;
+        int AvgDamage = (startAttack1 + startAttack2 + startAttack3)/3 ;
+        
+        System.out.println("Superman's Attack Damage is higher than Batman: " + HigherDamage);
+        System.out.println("The average attack damage of the three heroes is: " + AvgDamage);
+        
+        
     }
     
 }
